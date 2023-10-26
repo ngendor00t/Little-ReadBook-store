625b3d2d8a297074957bc17491305ecdcfffcbc8
Reginawambui <ngendoanita8@gmail.com>
2023-10-26 15:54:44 +0300

    complete

--- index.js
+++ index.js
@@ -2,35 +2,30 @@ function getPoems() {
     fetch("http://localhost:3000/poems")
       .then(response => response.json())
       .then(data => {
+        console.log(data)
         displayPoemDetails(data);
-        console.log(data);
       })
       .catch(error => {
         console.error("Error fetching poems:", error);
       });
   }
-
-// the function above is specifically used in fetching the data 
-
-
-function displayPoemDetails(data){
-for (let details of data){
-    console.log(details);
-let list = document.getElementById("books");
-let li =document.createElement('li');
-li.textContent= details.title
+  
+  function displayPoemDetails(details) {
+    const buyAbook = document.getElementById("buyAbook");
+    const booksList = document.getElementById("books");
+  
+    // Clear the previous list of books
+    booksList.innerHTML = "";
+  
+    details.forEach(poem => {
+      const poemElement = document.createElement("ul");
+      poemElement.innerHTML = `
+        <li>Title: ${poem.title}</li>
+        <li>Author: ${poem.author}</li>
+      <li>Lines: ${poem.lines}</li>
+      <button id="buyButton_${poem.id}">Buy a book</button>
+    `;
+    booksList.appendChild(poemElement);
+  });
 }
-}
-
-function displayPoemDetails(data){
-let buyAbook = document.getElementById("buyAbook");
-buyAbook.innerHTML=
-`<ul id= "books">
-<p>${details.title}</p>
-<p>author:${details.author}</p>
-<p>lines:${details.lines}</p>
-<button id="buyAbook">Buy a book</button>
-</ul>;
-`
-}
-
+getPoems()
